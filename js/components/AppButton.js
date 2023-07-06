50b7bec681f1d4b4ef2a0484ec2c085baeeec625
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2023-07-07 00:12:38 +0600

    new project pushed

--- js/components/AppButton.js
+++
@@ -1,5 +0,0 @@
-export default {
-    template: `
-        <button class="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded">Submit</button>
-    `,
-  }
---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<!-- <script setup>
+import { ref } from 'vue'
+
+let message = ref('Hello World!');
+
+</script>
+
+<template>
+
+  <div>
+    <h1>{{ message }}</h1>
+    <input type="text" name="" id="" v-model="message.value">
+  </div>
+
+</template> -->
+
+<script setup>
+import { ref, reactive } from 'vue'
+
+let name = ref('');
+
+let users = reactive([
+  { name: 'David' },
+  { name: 'Kevin' },
+]);
+
+const addUser = (event) => {
+  event.preventDefault();
+  let isAvailableUser =  users.filter((user) => user.name == name.value);
+  if(isAvailableUser.length > 0){
+    alert('User already Available');
+  } else {
+    users.push({name: name.value});
+  }
+
+};
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <input type="text" v-model="name">
+    <button @click="addUser">Add</button>
+    <ul>
+      <li v-for="(user, i) in users" :key="i">{{ user.name }}</li>
+    </ul>
+  </div>
+</template>
+
