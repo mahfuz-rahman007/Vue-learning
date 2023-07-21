acc6a187cb39e1570ae059cad69c6e69cf8bbb2d
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2023-07-21 18:43:32 +0600

    working on todo

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,88 @@
 <script setup>
-import { ref, reactive } from 'vue'
+import { ref, reactive, computed } from "vue";
 
-let name = ref('');
+let name = ref("");
 
-let users = reactive([
-  { name: 'David' },
-  { name: 'Kevin' },
+const todos = reactive([
+  { task: "Make Homework", complete: false, id: 1 },
+  { task: "Watch Movies", complete: false, id: 2 },
 ]);
 
-const addUser = (event) => {
+const addTodo = (event) => {
   event.preventDefault();
-  let isAvailableUser =  users.filter((user) => user.name == name.value);
-  if(isAvailableUser.length > 0){
-    alert('User already Available');
-  } else {
-    users.push({name: name.value});
+  if (!name.value.length) {
+    alert('Give a Valid Task');
+    return
   }
-
+  todos.push({
+      task: name.value,
+      complete: false,
+      id: todos.length + 1,
+    });
+    name.value = "";
 };
+
+const filter = computed(() => {
+  return {
+    incomplete: todos.filter((task) => !task.complete),
+    complete: todos.filter((task) => task.complete),
+  };
+});
 
 </script>
 
 <template>
   <div class="card card-body">
     <h2>Todo List</h2>
-    <div class="d-flex w-50 my-3">
-      <input type="text" v-model="name" class="form-control">
-      <button @click="addUser" class="btn btn-success">Add</button>
+    <div class="d-flex my-3">
+      <input
+        type="text"
+        v-model="name"
+        class="form-control"
+        placeholder="Create New Todo"
+      />
+      <button @click="addTodo" class="btn btn-success ml-2">Add</button>
     </div>
 
-    <ul class="list">
-      <li class="list-item" v-for="(user, i) in users" :key="i">
-        <label for="">
-          <input type="checkbox" name="check" id="">
-          {{ user.name }}
-        </label>
-      </li>
-    </ul>
+    <div class="my-3">
+      <h5>Incomplete List</h5>
+      <ul class="list-group" v-if="filter.incomplete.length">
+        <li
+          class="list-group-item"
+          v-for="(todo, i) in filter.incomplete"
+          :key="todo.id"
+        >
+          <label>
+            {{ i + 1 + " . " }}
+            <input type="checkbox" name="check" v-model="todo.complete" />
+            {{ todo.task }}
+          </label>
+        </li>
+      </ul>
+      <p class="alert alert-primary text-center" v-else>No Task</p>
+    </div>
+
+    <div class="my-3">
+      <h5>Complete List</h5>
+      <ul class="list-group" v-if="filter.complete.length">
+        <li
+          class="list-group-item"
+          v-for="(todo, i) in filter.complete"
+          :key="todo.id"
+        >
+          <label>
+            {{ i + 1 + " . " }}
+            <input
+              type="checkbox"
+              name="check"
+              checked
+              v-model="todo.complete"
+            />
+            <del class="mx-1"> {{ todo.task }} </del>
+          </label>
+        </li>
+      </ul>
+      <p class="alert alert-primary text-center" v-else>No Task</p>
+    </div>
   </div>
 </template>
